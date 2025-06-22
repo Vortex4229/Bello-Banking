315cc715d9e51b651df7b5424f8ecb509d6c2d13
Paulo Bello <pebrpaul@gmail.com>
2025-06-22 11:21:25 -0700

    Refactored the methods in AccountManagementRepo.cs and RootMethodsRepo.cs for use with the API.

--- Bello Banking API/RootMethodsRepo.cs
+++ Bello Banking API/RootMethodsRepo.cs
@@ -5,8 +5,6 @@ using System.Security.Cryptography;
 
 namespace Bello_Banking_API;
 
-// TODO: Refactor RootMethodsRepo functions to return a value instead of printing to console.
-
 public static class RootMethods {
 	
 	public static string PasswordEncryption(string password) {
@@ -15,11 +13,12 @@ public static class RootMethods {
 		return Encoding.ASCII.GetString(data);
 	}
 	
-	public static void Register(MySqlConnection conn, string username, string password, string email, string firstName, string lastName) {
+	public static bool Register(MySqlConnection conn, string username, string password, string email, string firstName, string lastName) {
 		var registrationQuery =
 			"INSERT INTO users(username, password, email, firstname, lastname, balance) values (@username, " +
 			"@password, @email, @firstname, @lastname, @balance)";
 		var registrationCmd = new MySqlCommand(registrationQuery, conn);
+		bool success = false;
 
 		try {
 			registrationCmd.Parameters.Add("@username", MySqlDbType.VarChar).Value = username;
@@ -30,39 +29,33 @@ public static class RootMethods {
 			registrationCmd.Parameters.Add("@balance", MySqlDbType.Int64).Value = 2000;
 			conn.Open();
 			registrationCmd.ExecuteNonQuery();
-			Console.WriteLine("Registration complete! Returning to main menu...");
-			Console.ReadLine();
+			success = true;
 		}
 		catch (MySqlException e) {
 			Console.WriteLine(e.Message);
-			Console.WriteLine("Registration failed, returning to main menu...");
-			Console.ReadLine();
 		}
 		finally {
 			if (conn.State == ConnectionState.Open) conn.Close();
 		}
+		
+		return success;
 	}
 	
 	public static (bool, ulong?) Login(MySqlConnection conn, string username, string password) {
-		var firstNameQuery = "SELECT firstName FROM users WHERE id=@id";
-		var lastNameQuery = "SELECT lastName FROM users WHERE id=@id";
+		
 		var accountIdQuery = "SELECT id FROM users WHERE username=@username AND password=@password";
 		var loginQuery =
 			"SELECT EXISTS (SELECT * FROM users WHERE username=@username AND password=@password) AS user_exists;";
-		
-		var loginSuccess = false;
-		string? firstName = null;
-		string? lastName = null;
-		ulong? accountId = null;
 
 		var loginCmd = new MySqlCommand(loginQuery, conn);
-		var firstNameCmd = new MySqlCommand(firstNameQuery, conn);
-		var lastNameCmd = new MySqlCommand(lastNameQuery, conn);
 		var accountIdCmd = new MySqlCommand(accountIdQuery, conn);
 		loginCmd.Parameters.Add("@username", MySqlDbType.VarChar).Value = username;
 		loginCmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = PasswordEncryption(password);
 		accountIdCmd.Parameters.Add("@username", MySqlDbType.VarChar).Value = username;
 		accountIdCmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = PasswordEncryption(password);
+		
+		var loginSuccess = false;
+		ulong? accountId = null;
 
 		try {
 			conn.Open();
@@ -71,37 +64,16 @@ public static class RootMethods {
 			while (loginCmdReader.Read())
 				loginSuccess = loginCmdReader.GetInt64(0) == 1;
 			loginCmdReader.Close();
-
-			if (loginSuccess == false) {
-				if (conn.State == ConnectionState.Open) conn.Close();
-				Console.WriteLine("Login failed, invalid username or password, returning to main menu...");
-				Console.ReadLine();
-			}
-			else {
+			
+			if (loginSuccess) {
 				var accountIdCmdReader = accountIdCmd.ExecuteReader();
 				while (accountIdCmdReader.Read())
 					accountId = accountIdCmdReader.GetUInt64(0);
 				accountIdCmdReader.Close();
-
-				firstNameCmd.Parameters.Add("@id", MySqlDbType.Int64).Value = accountId;
-				lastNameCmd.Parameters.Add("@id", MySqlDbType.Int64).Value = accountId;
-
-				var firstNameCmdReader = firstNameCmd.ExecuteReader();
-				while (firstNameCmdReader.Read())
-					firstName = firstNameCmdReader.GetString(0);
-				firstNameCmdReader.Close();
-
-				var lastNameCmdReader = lastNameCmd.ExecuteReader();
-				while (lastNameCmdReader.Read())
-					lastName = lastNameCmdReader.GetString(0);
-				lastNameCmdReader.Close();
-
-				Console.WriteLine($"Login successful! Welcome {firstName} {lastName}!");
 			}
 		}
 		catch (MySqlException e) {
 			Console.WriteLine(e.Message);
-			Console.WriteLine("Login failed, returning to main menu...");
 		}
 		finally {
 			if (conn.State == ConnectionState.Open) conn.Close();
